d34ca8ffe9f63762feaef2d94dfd2846af6645e2
Nityam Kumar <nityam950@gmail.com>
2024-04-08 10:40:01 +0530

    mail test

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -1,58 +1,20 @@
 "use client";
-import { Input } from "@/ui/Input";
-import { SubmitButton } from "@/ui/SubmitButton";
-import { useRouter } from "next/navigation";
-import { identity } from "@/server/authentication/identity";
-import { errorToast, successToast } from "@/hooks/useToast";
-import { useState } from "react";
 
-export default function CreateForm() {
-  const [otp, setOtp] = useState(false);
-  //   const router = useRouter();
-  async function identityCreate(formData: FormData) {
-    // const createIdentity = await identity(formData);
-    // if (createIdentity?.userexists) {
-    //   errorToast(createIdentity.userexists);
-    // }
-    // if (createIdentity?.success) {
-    //   successToast(createIdentity.success);
-    //   router.push("/");
-    // }
-    // if (createIdentity?.error) {
-    //   errorToast(createIdentity.error);
-    // }
-    setOtp(true);
+import { successToast } from "@/hooks/useToast";
+import { sendMail } from "@/server/mail/mail";
+import React from "react";
+
+export default function page() {
+  async function mail() {
+    const email = "iskip.dev@gmail.com";
+    const otp = "AEIOU";
+    const username = "inityamkr";
+    await sendMail(email, otp, username);
+    successToast("MAIL SENT");
   }
   return (
-    <div className="flex flex-col gap-2 justify-center items-center h-dvh">
-      {!otp && (
-        <form
-          action={identityCreate}
-          className="flex flex-col gap-3 justify-center items-center"
-        >
-          <Input type="text" defaultValue="CREATE" name="type" hidden />
-          <Input placeholder="email" type="email" name="email" required />
-          <Input
-            placeholder="username"
-            type="username"
-            name="username"
-            required
-          />
-          <Input
-            placeholder="password"
-            type="password"
-            name="password"
-            required
-          />
-          <SubmitButton text={"Create"} />
-        </form>
-      )}
-      {otp && (
-        <form className="flex flex-col gap-3 justify-center items-center">
-          <Input type="text" name="otp" placeholder="otp" />
-          <SubmitButton text={"Verify Email"} />
-        </form>
-      )}
+    <div>
+      <button onClick={mail}>send mail</button>
     </div>
   );
 }
